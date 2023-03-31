4cc7683e6100f78574b1a312b77e828afda033b1
Shaik Abdul Ahad <118795198+ShaikAbdul01@users.noreply.github.com>
2023-03-31 14:49:23 +0600

    error handle of blog card

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header/Header'
-import Card from './Components/Card/Card'
 import BlogCard from './Components/BlogCard/BlogCard'
 
 function App() {
